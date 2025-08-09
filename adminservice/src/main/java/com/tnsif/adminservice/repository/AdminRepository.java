c8fe1fdbdab1c76653c985cb0cb91979ab42ec36
Admin <Admin@DSE-UNAA3SI03840504FBC0700>
2025-08-09 22:01:54 +0530

    Sprint1 project

---
+++ adminservice/src/main/java/com/tnsif/adminservice/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.tnsif.adminservice.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+}
+
